92fd028cacc8fa4dfb46767b0c2286f0f2cf7853
Nathan Keith <nashvegasnate@gmail.com>
2021-03-10 15:19:11 -0600

    deleted divs where login/logout buttons were being hidden in home.js and navBar.js - fixed those issues

--- src/javascripts/components/home.js
+++ src/javascripts/components/home.js
@@ -5,13 +5,12 @@ const home = () => {
   document.querySelector('#navigation').innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#"><img src="https://i.pinimg.com/originals/d3/d1/75/d3d175e560ae133f1ed5cd4ec173751a.png" width="30" height="30" alt="pinterest logo">PINTEREST-ISH
+        <a class="navbar-brand" href="#"><img src="https://i.pinimg.com/originals/d3/d1/75/d3d175e560ae133f1ed5cd4ec173751a.png" width="30" height="30" alt="pinterest logo">PINTEREST-ish
         </a>
       </div>
+      <div id="login-form-container" d-flex></div>
     </nav>
-    <div id="main-container>
-      <div id="login-form-container"></div>
-    </div>`;
+    `;
 };
 
 export default home;
